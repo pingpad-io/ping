e708609d404e5e52d2f79828c197339b77368ed5
Mathew <contact@kualta.dev>
2023-03-25 13:57:49 +0300

    add posts router

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,13 @@
-import { SignIn, SignInButton } from "@clerk/nextjs";
+import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
 import { type NextPage } from "next";
 import Head from "next/head";
-import Link from "next/link";
 
 import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
+  const user = useUser();
+
+  const { data } = api.posts.getAll.useQuery();
 
   return (
     <>
@@ -15,9 +16,16 @@ const Home: NextPage = () => {
         <meta name="description" content="an anonymised twitter" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-        {/* <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" /> */}
-        <SignInButton />
+
+      <main className="dark flex min-h-screen flex-col items-center justify-center bg-[#101112] dark:text-stone-200">
+        {user.isSignedIn && <SignOutButton />}
+        {!user.isSignedIn && <SignInButton />}
+
+        <div>
+          {data?.map((post) => (
+            <div key={post.id}>{post.content}</div>
+          ))}
+        </div>
       </main>
     </>
   );
