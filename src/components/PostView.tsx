0648e91bbc6384940b12b34fe91f65781a29b353
Mathew <contact@kualta.dev>
2023-03-27 11:19:49 +0300

    add user id tip

--- src/components/PostView.tsx
+++ src/components/PostView.tsx
@@ -1,3 +1,4 @@
+import { useUser } from "@clerk/nextjs";
 import { Post } from "@prisma/client";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
@@ -5,15 +6,25 @@ import relativeTime from "dayjs/plugin/relativeTime";
 dayjs.extend(relativeTime);
 
 export const PostView = ({ post }: { post: Post }) => {
-  let date = post.createdAt.toLocaleDateString();
+  const { user } = useUser();
+
+  let date = post.createdAt.toLocaleDateString(); // TODO: add tooltip
   let time = dayjs(post.createdAt).fromNow();
   let id = post.authorId.substring(post.authorId.length - 7);
-  let user = "@" + id.toLocaleLowerCase();
+  let username = "@" + id.toLocaleLowerCase();
+  let youText =
+    user?.id === post.authorId ? (
+      <span className="text-slate-400">(you)</span>
+    ) : (
+      <></>
+    );
 
   return (
     <div className="mt-2 border-b-2 border-slate-700 p-4">
       <div className="flex flex-row gap-2 text-sm">
-        <div className="">{user}</div>
+        <div className="">
+          {username} {youText}
+        </div>
         <div>·</div>
         <div className="">{time}</div>
       </div>
