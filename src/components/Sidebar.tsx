506535529b4e8a40dc4463c6c595f4cfa99451ba
Mathew <contact@kualta.dev>
2023-04-04 23:41:53 +0300

    update sidemenu

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,28 +1,35 @@
 import { FiGlobe, FiMessageCircle, FiSearch } from "react-icons/fi";
 import { CollapsedContext } from "./Menu";
 import { MenuItem } from "./MenuItem";
+import { BsNewspaper } from "react-icons/bs";
 
 const LatestNews = () => {
   return (
-    <div className="card hidden bg-base-200 p-4 xl:flex">
-      <div className="card-title text-lg">Latest Update</div>
-      Global themes! New menu. Updated profile view. Updated suspense.
-    </div>
+    <>
+      <div className="sm:flex xl:hidden">
+        <MenuItem name={""} icon={<BsNewspaper />}></MenuItem>
+      </div>
+
+      <div className="card bg-base-200 p-4 hidden xl:flex">
+        <div className="card-title text-lg">Latest Update</div>
+        Global themes! New menu. Updated profile view. Updated suspense.
+      </div>
+    </>
   );
 };
 
 const Threads = () => {
   return (
     <>
-      <div className="hidden flex-col gap-2 sm:flex xl:hidden">
+      <div className="flex-col gap-2 sm:flex xl:hidden">
         <MenuItem name={""} icon={<FiMessageCircle />}></MenuItem>
         <MenuItem name={""} icon={<FiGlobe />}></MenuItem>
       </div>
 
-      <div className="hidden flex-col justify-center xl:flex">
-        <div className="mt-8 text-center text-lg">Threads</div>
-        <div className="items-left flex flex-col">
-          <div className="font-mono">· Global</div>
+      <div className="card hidden flex-col justify-center bg-base-300 p-4 xl:flex">
+        <div className="card-title">Threads</div>
+        <div className="flex flex-col">
+          <div className="link-hover link p-4">- Global</div>
         </div>
       </div>
     </>
@@ -48,7 +55,7 @@ const SearchBar = () => {
 
 export default function Sidebar() {
   return (
-    <div className="sticky top-0 hidden h-screen w-max max-w-xs shrink flex-col gap-2 py-4 px-2 sm:flex">
+    <div className="sticky top-0 hidden h-screen w-max max-w-xs shrink flex-col gap-2 py-4 px-2 sm:flex xl:gap-4">
       <CollapsedContext.Provider value={true}>
         <SearchBar />
         <LatestNews />
