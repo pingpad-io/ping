d9872a9fdb696d2a141e462f9a7cdd3193d242a0
kualta <contact@kualta.dev>
2023-10-17 11:41:15 +0300

    update middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,19 +3,30 @@ import { createMiddlewareSupabaseClient } from "@supabase/auth-helpers-nextjs";
 
 // Stop Middleware running on static files
 export const config = {
-  matcher: "/((?!_next/image|_next/static|favicon.ico|api).*)",
+  matcher: "/((?!api|_next/image|_next/static|favicon.ico|api).*)",
 };
 
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next();
   const supabase = createMiddlewareSupabaseClient({ req, res });
+  const pathname = req.nextUrl.pathname;
 
   const {
     data: { session },
   } = await supabase.auth.getSession();
 
   if (session?.user) {
-    // Authentication successful, forward request to protected route.
+    // Auth successful, forward request to protected route.
+    return res;
+  }
+
+  if (pathname === "/policy" || pathname === "/conditions" || pathname === "/about") {
+    // Allow open routes
+    return res;
+  }
+
+  if (pathname.match(/^\/p\/.*/)) {
+    // Individual posts are open too
     return res;
   }
 
