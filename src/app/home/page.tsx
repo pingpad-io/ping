5023e5b6c17647ba5a282ea2af1a988e1cd01dc2
kualta <contact@kualta.dev>
2024-04-05 12:26:38 +0300

    add client, add local storage

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,17 +1,28 @@
+import { LensClient, production } from "@lens-protocol/client";
 import { HomePage } from "./HomePage";
+import { localStorage } from "~/utils/storage";
 
 const home = async () => {
-  // const post = posts?.map((publication) => publication.by.id);
+  const client = new LensClient({ environment: production, storage: localStorage() });
+  const publications = await client.publication.fetchAll({ where: { from: ["0x04359b"] } });
 
-  // console.log(post);
+  // console.log(`Is LensClient authenticated? `, await client.authentication.isAuthenticated());
+  // console.log(`Authenticated profileId: `, profileId);
+  // console.log(`Access token: `, accessToken);
+  // console.log(`Is access token valid? `, await client.authentication.verify({ accessToken }));
+  // const post = posts?.map((publication) => publication.by.id);
+  const posts = publications.items.map((publication) => publication.by.metadata.bio);
+
+  console.log(publications.items);
   return (
     <>
       {/* <Card className="z-[30] sticky top-0 flex-col hidden sm:flex flex-none p-4 border-0"> */}
       {/* <PostWizard /> */}
       {/* </Card> */}
       {/* {publications.items} */}
-      {/* {post} */}
+      {posts}
 
+      {/* {publications.items} */}
       {/* <Feed {...posts} /> */}
       <HomePage />
     </>
---
+++ src/utils/globals.ts
@@ -0,0 +1,9 @@
+import { maybe, never } from '@lens-protocol/shared-kernel';
+
+const safeGlobal = (maybe(() => globalThis) ??
+  maybe(() => window) ??
+  maybe(() => self) ??
+  maybe(() => global) ??
+  never('Cannot resolve a global object.')) as typeof globalThis & Window;
+
+export const window = maybe(() => safeGlobal.window) ?? null;
