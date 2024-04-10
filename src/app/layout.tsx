490924824001522f327dbcdde982085fbd4b269f
kualta <contact@kualta.dev>
2024-04-10 13:37:27 +0300

    add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,8 @@ import { usePathname } from "next/navigation";
 import "../styles/globals.css";
 import dynamic from "next/dynamic";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
+
 const Providers = dynamic(() => import("../components/Providers"), { ssr: false });
 
 // export const metadata = {
@@ -33,6 +35,7 @@ export default function RootLayout({
     <html className={`${quicksand.variable} scroll-smooth font-sans`} lang="en">
       <body className="flex flex-col">
         <SpeedInsights />
+        <Analytics />
 
         <Providers>
           <div className="flex flex-row justify-center shrink grow w-full shrink">
