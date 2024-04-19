3ff1b3a43f2a967dca446ba5c4ab7fc61144b198
kualta <contact@kualta.dev>
2024-04-19 20:12:11 +0300

    add settings page

---
+++ src/app/settings/page.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import { useTheme } from "next-themes";
+import { useRouter } from "next/navigation";
+import { SignedIn, SignedOut } from "~/components/Authenticated";
+import { Button } from "~/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "~/components/ui/card";
+
+export const SettingsPage = () => {
+  const { setTheme } = useTheme();
+  const router = useRouter();
+
+  const signOut = async () => { };
+
+  const themeButtons = ["light", "dark"].map((theme) => (
+    <Button
+      data-theme={theme}
+      type="submit"
+      key={theme}
+      variant={theme === "dark" ? "secondary" : "outline"}
+      className=""
+      onClick={() => setTheme(theme)}
+    >
+      {theme}
+    </Button>
+  ));
+
+  return (
+    <div className="p-4 space-y-4">
+      <SignedIn>
+        {/* <ProfileSettings profile={profile} /> */}
+
+        <Card>
+          <CardHeader>
+            <CardTitle>Theme</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <span className="flex gap-2">{themeButtons}</span>
+          </CardContent>
+        </Card>
+
+        <Card>
+          <CardHeader>
+            <CardTitle>Danger Zone</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <Button type="button" variant="destructive" onClick={() => signOut()}>
+              Sign Out
+            </Button>
+          </CardContent>
+        </Card>
+      </SignedIn>
+
+      <SignedOut>
+        <h1 className="m-8 flex flex-row items-center justify-center p-8 text-xl">Sign in to view this page</h1>
+      </SignedOut>
+    </div>
+  );
+};
