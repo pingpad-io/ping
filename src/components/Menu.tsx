c20c494d3f45830030d815121a579a7950309292
kualta <contact@kualta.dev>
2024-04-26 12:12:53 +0300

    ssr for menu

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,23 +1,16 @@
-"use client";
-
-import { Profile, SessionType, useSession } from "@lens-protocol/react-web";
 import { AtSign, BellIcon, MailIcon, SearchIcon, SendIcon, SettingsIcon, UserIcon } from "lucide-react";
+import { cookies } from "next/headers";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
-import { createContext } from "react";
-import { toast } from "react-hot-toast";
-import { useAccount } from "wagmi";
 import { Button } from "~/components/ui/button";
 import { ConnectWalletButton } from "./web3/WalletButtons";
 
-export const CollapsedContext = createContext(false);
-
 export default function Menu() {
-  const _router = useRouter();
-  const { data: session, error, loading } = useSession();
-  const { isConnected: connected } = useAccount();
+  const cookieStore = cookies();
+  const handle = cookieStore.get("handle")?.value;
+  const profileId = cookieStore.get("profileId")?.value;
+  const handleOrProfileId = handle ?? profileId;
 
-  if (loading) {
+  if (!profileId) {
     return (
       <span className="flex shrink text-xl p-4 sm:px-2 w-full sm:w-max">
         <span className="flex flex-row sm:flex-col items-end gap-2 place-content-between sm:place-content-start w-full">
@@ -31,13 +24,6 @@ export default function Menu() {
       </span>
     );
   }
-
-  if (error) {
-    console.log(error);
-    return null;
-  }
-
-  const authed = connected && session?.authenticated && session.type === SessionType.WithProfile;
 
   return (
     <span className="flex shrink text-xl p-4 sm:px-2 w-full sm:w-max">
@@ -60,25 +46,21 @@ export default function Menu() {
           {/* )} */}
         </div>
 
-        {authed ? <MenuAuthed profile={session.profile} /> : <ConnectWalletButton />}
+        {profileId ? <MenuAuthed handle={handleOrProfileId} /> : <ConnectWalletButton />}
       </span>
     </span>
   );
 }
 
-export const MenuAuthed = ({ profile }: { profile: Profile }) => {
-  const todo = () => {
-    toast.error("coming soon™");
-  };
-
+export const MenuAuthed = ({ handle }: { handle: string }) => {
   return (
     <>
-      <Button variant="ghost" size="sm_icon" onClick={todo}>
+      <Button variant="ghost" size="sm_icon" disabled>
         <div className="hidden sm:flex -mt-1">messages</div>
         <MailIcon className="sm:ml-2" size={20} />
       </Button>
 
-      <Button variant="ghost" size="sm_icon" onClick={todo}>
+      <Button variant="ghost" size="sm_icon" disabled>
         <div className="hidden sm:flex -mt-1">notifications</div>
         <BellIcon className="sm:ml-2" size={21} />
       </Button>
@@ -90,14 +72,14 @@ export const MenuAuthed = ({ profile }: { profile: Profile }) => {
         </Button>
       </Link>
 
-      {/* <Link href={`/u/${profile.handle.fullHandle.split("/")[1].toLowerCase()}`}> */}
-      <Button variant="ghost" size="sm_icon" onClick={todo}>
-        <div className="hidden sm:flex -mt-1">profile</div>
-        <UserIcon className="sm:ml-2" size={21} />
-      </Button>
-      {/* </Link> */}
+      <Link href={`/u/${handle}`}>
+        <Button variant="ghost" size="sm_icon" disabled>
+          <div className="hidden sm:flex -mt-1">profile</div>
+          <UserIcon className="sm:ml-2" size={21} />
+        </Button>
+      </Link>
 
-      <Button onClick={todo}>
+      <Button disabled>
         <div className="hidden sm:flex -mt-1">post</div>
         <SendIcon className="sm:ml-2" size={20} />
       </Button>
