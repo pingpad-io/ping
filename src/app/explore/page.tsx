c6edeca187f8e4bd2a0383fdb5fc37d0f14bf8f8
kualta <contact@kualta.dev>
2024-04-26 11:30:02 +0300

    add auth check to /explore

--- src/app/explore/page.tsx
+++ src/app/explore/page.tsx
@@ -12,17 +12,18 @@ const lensClient = new LensClient({
 const home = async () => {
   const cookieStore = cookies();
   const refreshToken = cookieStore.get("refreshToken").value;
+
+  if (!refreshToken) return <ErrorPage title="Login to view this page." />;
   await lensClient.authentication.authenticateWith({ refreshToken });
   const data = await lensClient.feed.fetch({
     where: {
       for: "0x01",
-    }
+    },
   });
 
   const suspense = [...Array(12)].map((_v, idx) => <SuspenseView key={`suspense-${idx}`} />);
-
   if (!data.isSuccess()) return suspense;
-  if (data.isFailure()) return <ErrorPage title={`Couldn't fetch posts: ${data.error} `}/>;
+  if (data.isFailure()) return <ErrorPage title={`Couldn't fetch posts: ${data.error} `} />;
 
   const items = data.unwrap().items;
   const posts = items.map((publication) => lensItemToPost(publication)).filter((post) => post);
