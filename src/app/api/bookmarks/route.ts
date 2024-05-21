6107eff6555680d22046eefe7b07001f495e8129
kualta <contact@kualta.dev>
2024-05-21 10:00:32 +0000

    add bookmarks api, add /bookmarks route

--- src/app/api/bookmarks/route.ts
+++ src/app/api/bookmarks/route.ts
@@ -1,5 +1,6 @@
 import type { NextRequest } from "next/server";
 import { lensNotificationToNative } from "~/components/notifications/Notification";
+import { lensItemToPost } from "~/components/post/Post";
 import { getLensClient } from "~/utils/getLensClient";
 
 export const dynamic = "force-dynamic";
@@ -15,16 +16,16 @@ export async function GET(req: NextRequest) {
       return new Response(JSON.stringify({ error: "Not authenticated" }), { status: 401 });
     }
 
-    const data = await client.notifications.fetch({ where: { timeBasedAggregation: true }, cursor });
+    const data = await client.publication.bookmarks.fetch( {cursor} );
 
     if (!data.isSuccess()) {
-      return new Response(JSON.stringify({ error: "Failed to fetch notifications" }), { status: 500 });
+      return new Response(JSON.stringify({ error: "Failed to fetch bookmarks" }), { status: 500 });
     }
 
-    const posts = data.unwrap().items.map(lensNotificationToNative);
+    const posts = data.unwrap().items.map(lensItemToPost);
 
     return new Response(JSON.stringify({ posts, nextCursor: data.unwrap().pageInfo.next }), { status: 200 });
   } catch (error) {
-    return new Response(JSON.stringify({ error: `Failed to fetch notifications: ${error.message}` }), { status: 500 });
+    return new Response(JSON.stringify({ error: `Failed to fetch bookmarks: ${error.message}` }), { status: 500 });
   }
 }
--- src/app/bookmarks/page.tsx
+++ src/app/bookmarks/page.tsx
@@ -1,27 +1,45 @@
-import { NotificationsFeed } from "~/components/Feed";
+import { BookmarkIcon } from "lucide-react";
+import { FeedPageLayout } from "~/components/FeedPagesLayout";
+import { InfiniteScroll } from "~/components/InfiniteScroll";
 import { lensNotificationToNative } from "~/components/notifications/Notification";
+import { lensItemToPost } from "~/components/post/Post";
 import { Card } from "~/components/ui/card";
 import { getLensClient } from "~/utils/getLensClient";
 
-const bookmarks = async () => {
-  const { client, isAuthenticated } = await getLensClient();
+const endpoint = "/api/bookmarks";
 
+const notifications = async () => {
+  const { bookmarks, nextCursor } = await getInitialFeed();
+
+  if (!bookmarks) {
+    throw new Error("Failed to get bookmarks (╥_╥)");
+  }
+
+  return (
+    <Card className="z-[30] hover:bg-card p-4 py-0 border-0">
+      <h1 className="text-xl font-bold p-4 flex flex-row gap-2 items-center">
+        <BookmarkIcon className="-mb-1" fill="hsl(var(--foreground))" /> bookmarks
+      </h1>
+      <InfiniteScroll endpoint={endpoint} initialData={bookmarks} initialCursor={nextCursor} />
+    </Card>
+  );
+};
+
+const getInitialFeed = async () => {
+  const { client, isAuthenticated } = await getLensClient();
   if (isAuthenticated) {
-    const data = await client.notifications.fetch({ where: { timeBasedAggregation: true } }).catch((error) => {
+    const data = await client.publication.bookmarks.fetch({}).catch((error) => {
       throw new Error(error.message);
     });
 
     if (data.isFailure()) throw new Error(data.error.message);
 
-    const items = data.unwrap().items;
-    const notifications = items?.map((notification) => lensNotificationToNative(notification));
+    const items = data.unwrap();
+    const bookmarks = items.items?.map(lensItemToPost);
 
-    return (
-      <Card className="z-[30] hover:bg-card p-4 border-0">
-        <NotificationsFeed data={notifications} />
-      </Card>
-    );
+    return { bookmarks, nextCursor: items.pageInfo.next };
   }
+  throw new Error("Unauthorized TT");
 };
 
-export default bookmarks;
+export default notifications;
