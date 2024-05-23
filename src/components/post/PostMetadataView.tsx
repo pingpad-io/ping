d099a565c26effe712990438b97afd97f32f6f41
kualta <contact@kualta.dev>
2024-05-23 08:45:57 +0000

    add video support

--- src/components/post/PostMetadataView.tsx
+++ src/components/post/PostMetadataView.tsx
@@ -1,4 +1,10 @@
-import type { ArticleMetadataV3, ImageMetadataV3, TextOnlyMetadataV3 } from "@lens-protocol/react-web";
+import type {
+  ArticleMetadataV3,
+  AudioMetadataV3,
+  ImageMetadataV3,
+  TextOnlyMetadataV3,
+  VideoMetadataV3,
+} from "@lens-protocol/react-web";
 import Image from "next/image";
 import Markdown from "../Markdown";
 import type { AnyLensMetadata } from "./Post";
@@ -11,6 +17,10 @@ export const getPostMetadataView = (metadata: AnyLensMetadata) => {
       return <ArticleView metadata={metadata} />;
     case "ImageMetadataV3":
       return <ImageView metadata={metadata} />;
+    case "VideoMetadataV3":
+      return <VideoView metadata={metadata} />;
+    case "AudioMetadataV3":
+      return <AudioView metadata={metadata} />;
     default:
       return null;
   }
@@ -38,4 +48,24 @@ export const ImageView = ({ metadata }: { metadata: ImageMetadataV3 }) => {
       </div>
     </div>
   );
+};
+
+export const VideoView = ({ metadata }: { metadata: VideoMetadataV3 }) => {
+  const url = metadata.asset.video.optimized.uri || metadata.asset.video.raw.uri;
+  const mimeType = metadata.asset.video.optimized.mimeType || metadata.asset.video.raw.mimeType;
+  const cover = metadata.asset.cover?.optimized.uri || metadata.asset.cover?.raw.uri;
+  console.log(cover, mimeType);
+  return (
+    <div>
+      <Markdown content={metadata.content} />
+      <video className="w-full h-auto rounded-lg" autoPlay muted loop playsInline controls poster={cover}>
+        <source src={url} type={mimeType} />
+        Your browser does not support the video tag.
+      </video>
+    </div>
+  );
+};
+
+export const AudioView = ({ metadata }: { metadata: AudioMetadataV3 }) => {
+  return <Markdown content={metadata.content} />;
 };
