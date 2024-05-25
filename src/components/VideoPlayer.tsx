fd4f9383300172d3c2aea8ea4cf50339927af18a
kualta <contact@kualta.dev>
2024-05-25 08:29:06 +0000

    video player improvements

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -1,17 +1,20 @@
+"use client";
+
 import { PauseIcon, PlayIcon } from "lucide-react";
 import { useRef, useState } from "react";
 import ReactPlayer from "react-player";
-import { Progress } from "./ui/progress";
+import { Progress } from "./ui/video-progress";
 
 export const VideoPlayer = ({ url }) => {
   const playerRef = useRef(null);
-  const playerContainerRef = useRef(null);
   const progressRef = useRef(null);
-  const [playing, setPlaying] = useState(true);
+  const [playing, setPlaying] = useState(false);
   const [progress, setProgress] = useState(0);
+  const [muted, setMuted] = useState(true);
 
   const handlePlayPause = () => {
     setPlaying(!playing);
+    setMuted(false);
   };
 
   const handleProgress = (state) => {
@@ -20,30 +23,50 @@ export const VideoPlayer = ({ url }) => {
 
   const handleSeekChange = (value: number) => {
     playerRef.current.seekTo(value / 100);
+    setProgress(value);
   };
 
   return (
     <div className="relative rounded-xl w-full border">
-      <ReactPlayer
-        ref={playerRef}
-        url={url}
-        playing={playing}
-        onProgress={handleProgress}
-        width="100%"
-        height="auto"
-        loop
-        muted
-        style={{ borderRadius: "0.5rem", overflow: "hidden" }}
-        controls={false} // Disable default controls to use custom controls
-      />
+      <div
+        onClick={handlePlayPause}
+        onKeyDown={(e) => {
+          if (e.key === " ") {
+            handlePlayPause();
+          }
+        }}
+      >
+        <ReactPlayer
+          ref={playerRef}
+          playing={playing}
+          style={{ borderRadius: "0.5rem", overflow: "hidden" }}
+          onProgress={handleProgress}
+          progressInterval={50}
+          url={url}
+          width="100%"
+          height="auto"
+          loop
+          light={<div className="bg-black h-64 w-full opacity-50" />}
+          muted={muted}
+          controls={false} // disable default controls to use custom controls
+        />
+      </div>
 
-      <div className="z-10 w-full rounded-b-lg border-t absolute bottom-0 flex justify-between items-center backdrop-blur-lg text-foreground p-2">
+      <div
+        className="z-10 w-full rounded-b-lg border-t absolute bottom-0 flex justify-between 
+                   items-center backdrop-blur-sm text-secondary-foreground p-2 bg-secondary/50"
+      >
         <button type="button" onClick={handlePlayPause} className="cursor-pointer">
           {playing ? <PauseIcon /> : <PlayIcon />}
         </button>
-        {/* <input type="range" min="0" max="100" value={progress} onChange={handleSeekChange} className="flex-grow ml-2" /> */}
-
-        <Progress ref={progressRef} onChange={handleSeekChange} className="ml-2 h-2" value={progress} />
+        <Progress
+          ref={progressRef}
+          onChange={handleSeekChange}
+          playing={playing}
+          setPlaying={setPlaying}
+          className="ml-2 h-2"
+          value={progress}
+        />
       </div>
     </div>
   );
---
+++ src/components/ui/video-progress.tsx
@@ -0,0 +1,76 @@
+"use client"
+
+import { cn } from "@/src/utils";
+import * as ProgressPrimitive from "@radix-ui/react-progress";
+import * as React from "react";
+
+interface ProgressProps {
+  className?: string;
+  value?: number;
+  onChange?: (newValue: number) => void;
+  playing?: boolean;
+  setPlaying?: (playing: boolean) => void;
+}
+
+const Progress = React.forwardRef<HTMLDivElement, ProgressProps>(
+  ({ className, value, onChange, playing, setPlaying, ...props }, ref) => {
+    const [indicatorPosition, setIndicatorPosition] = React.useState<number>(value || 0);
+
+    React.useEffect(() => {
+      if (value !== undefined) {
+        setIndicatorPosition(value);
+      }
+    }, [value]);
+
+    const handleMouseDown = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+      e.preventDefault();
+      e.stopPropagation();
+
+      const wasPlaying = playing;
+      setPlaying?.(false);
+
+      const rect = (ref as React.MutableRefObject<HTMLDivElement>).current.getBoundingClientRect();
+      if (!rect) return;
+
+      const handleMouseMove = (e: MouseEvent) => {
+        e.stopPropagation();
+        const offsetX = e.clientX - rect.left;
+        const newValue = (offsetX / rect.width) * 100;
+        onChange?.(newValue);
+        setIndicatorPosition(newValue);
+      };
+
+      const handleMouseUp = () => {
+        setPlaying?.(wasPlaying);
+        document.removeEventListener("mousemove", handleMouseMove);
+        document.removeEventListener("mouseup", handleMouseUp);
+      };
+
+      document.addEventListener("mousemove", handleMouseMove);
+      document.addEventListener("mouseup", handleMouseUp);
+
+      handleMouseMove(e.nativeEvent as MouseEvent);
+    };
+
+    return (
+      <ProgressPrimitive.Root
+        ref={ref}
+        onMouseDown={handleMouseDown}
+        className={cn(
+          "z-[20] relative h-4 w-full transition-none rounded-full overflow-hidden bg-secondary cursor-pointer",
+          className,
+        )}
+        {...props}
+      >
+        <ProgressPrimitive.Indicator
+          className="h-full w-full flex-1 bg-primary relative"
+          style={{ transform: `translateX(-${100 - indicatorPosition}%)` }}
+        />
+      </ProgressPrimitive.Root>
+    );
+  },
+);
+
+Progress.displayName = ProgressPrimitive.Root.displayName;
+
+export { Progress };
