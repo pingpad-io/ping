aabd68e07704575e79c1b0b34373afb0523abb4f
kualta <contact@kualta.dev>
2024-06-13 11:49:08 +0000

    add new navigation

--- src/components/menu/Navigation.tsx
+++ src/components/menu/Navigation.tsx
@@ -1,43 +1,44 @@
 "use client";
 
+import { ClockIcon, CrownIcon, HomeIcon, NewspaperIcon, PlusCircleIcon, TelescopeIcon } from "lucide-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 export const Navigation = () => {
-  const pathname = usePathname();
-  const selectedStyle = (path: string) => (path === pathname ? "font-bold underline underline-offset-4" : "");
-
-  if (pathname.split("/")[1] === "explore") {
-    return <ExploreNavigation />;
-  }
-
   return (
-    <nav className="z-[40] flex flex-row justify-around items-center p-4 sticky top-0 backdrop-blur-md rounded-b-lg">
-      <Link className={selectedStyle("/home")} href={"/home"}>
-        For you
-      </Link>
-      <Link className={selectedStyle("/best")} href={"/best"}>
-        Best
-      </Link>
+    <nav className="z-[40] flex flex-row justify-start items-center p-2 gap-2 sticky top-0 backdrop-blur-md rounded-b-lg">
+      <NavigationItem href={"/home"}>
+        <HomeIcon size={18} />
+      </NavigationItem>
+      <NavigationItem href={"/best"}>
+        <CrownIcon size={18} />
+      </NavigationItem>
+      <NavigationItem href={"/explore/curated"}>
+        <NewspaperIcon size={18} />
+      </NavigationItem>
+      <NavigationItem href={"/explore/collected"}>
+        <PlusCircleIcon size={18} />
+      </NavigationItem>
+      <NavigationItem href={"/explore/latest"}>
+        <ClockIcon size={18} />
+      </NavigationItem>
     </nav>
   );
 };
 
-const ExploreNavigation = () => {
+export const NavigationItem = ({ children, href }) => {
   const pathname = usePathname();
-  const selectedStyle = (path: string) => (path === pathname ? "font-bold underline underline-offset-4" : "");
+  const selectedStyle = (path: string) =>
+    path === pathname ? "border-2 font-bold bg-accent text-accent-foreground" : "";
 
   return (
-    <nav className="z-[40] flex flex-row justify-around items-center p-4 sticky top-0 backdrop-blur-md rounded-b-lg">
-      <Link className={selectedStyle("/explore/curated")} href={"/explore/curated"}>
-        Curated
-      </Link>
-      <Link className={selectedStyle("/explore/collected")} href={"/explore/collected"}>
-        Collected
-      </Link>
-      <Link className={selectedStyle("/explore/latest")} href={"/explore/latest"}>
-        Latest
-      </Link>
-    </nav>
+    <Link
+      className={`rounded-full border aspect-square w-10 h-10 overflow-hidden inline-flex items-center justify-center text-sm font-medium ring-offset-background transition-colors hover:bg-muted 
+  hover:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 
+      ${selectedStyle(href)}`}
+      href={href}
+    >
+      {children}
+    </Link>
   );
 };
