de5e453c6caba9bc5af6174a3791fed5a9355688
kualta <contact@kualta.dev>
2024-06-18 06:27:13 +0000

    increase fade duration on landing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -41,23 +41,23 @@ const LandingPage = () => {
 
         <div className="grid md:grid-cols-2 px-auto grow items-center justify-center py-10 mt-16 text-3xl p-8 text-center drop-shadow-md dark:drop-shadow-glow place-items-center flex flex-col gap-6 justify-center">
           <div className="col-span-1 flex flex-col gap-2">
-            <FadeIn className="duration-500 delay-100">
+            <FadeIn className="duration-1000 delay-100">
               <h1>
                 a <b>better </b> decentralized social,
               </h1>
             </FadeIn>
-            <FadeIn className="duration-500 delay-300">
+            <FadeIn className="duration-1000 delay-300">
               <h1>
                 staying <b>out of the way</b>
               </h1>
             </FadeIn>
-            <FadeIn className="duration-500 delay-500">
+            <FadeIn className="duration-1000 delay-500">
               <h1>
                 to reach <b>your</b> people
               </h1>
             </FadeIn>
           </div>
-          <FadeIn className="duration-500 delay-1000">
+          <FadeIn className="duration-1000 delay-1000">
             <div className="col-span-1 flex flex-row gap-2 items-center justify-center">
               <h1>built on</h1>
               <Link className="hover:underline -mt-24 -mb-24  flex items-center gap-2" href={"https://lens.xyz"}>
@@ -72,7 +72,7 @@ const LandingPage = () => {
           </FadeIn>
         </div>
 
-        <FadeIn className="duration-1000 delay-[1500ms]">
+        <FadeIn className="duration-[3000ms] delay-[4500ms]">
           <div className="w-full p-8 mt-16 text-center drop-shadow-lg dark:drop-shadow-glow flex flex-col justify-center items-center">
             <div className=" text-xl flex flex-col gap-4">
               Join Waitlist
@@ -81,7 +81,7 @@ const LandingPage = () => {
           </div>
         </FadeIn>
 
-        <FadeIn className="duration-1000 delay-[2000ms]">
+        <FadeIn className="duration-[3000ms] delay-[5000ms]">
           <Card className="hover:bg-card flex flex-col gap-4 place-items-center mx-auto max-w-3xl p-4 my-32">
             <CardHeader>
               <CardTitle> FAQ </CardTitle>
@@ -117,7 +117,7 @@ const LandingPage = () => {
           </Card>
         </FadeIn>
 
-        <FadeIn className="duration-1000">
+        <FadeIn className="duration-[3000ms] delay-[5500ms]">
           <div className="flex flex-col md:flex-row gap-4 md:gap-8 place-items-center justify-center p-4 drop-shadow-lg dark:drop-shadow-glow my-20">
             <Link href="https://kualta.dev">
               <Button variant="ghost" className="p-1 px-4 text-lg gap-4 flex flex-row w-fit rounded-full">
