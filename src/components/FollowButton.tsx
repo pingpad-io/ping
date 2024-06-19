17c9732f1fa811ae3c509bb42afeafda6f9a0f8c
kualta <contact@kualta.dev>
2024-06-19 11:06:53 +0000

    restart effect on refollow

--- src/components/FollowButton.tsx
+++ src/components/FollowButton.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useRef, useState } from "react";
 import Explosion from "react-canvas-confetti/dist/presets/explosion";
 import { toast } from "sonner";
 import type { User } from "./post/Post";
@@ -8,25 +8,36 @@ import { Button } from "./ui/button";
 
 export const FollowButton = ({ user }: { user: User }) => {
   const [following, setFollowing] = useState(user.actions.followed);
-  const [showEffect, setShowEffect] = useState(false);
+  const controller = useRef<any>();
+
+  const onInitHandler = ({ conductor }) => {
+    controller.current = conductor;
+  };
+
+  const shootEffect = () => {
+    if (!controller.current) return;
+    controller.current.shoot();
+  };
 
   const toggleFollow = async () => {
     const endpoint = following ? "/api/profile/unfollow" : "/api/profile/follow";
 
-    if (!following) {
-      setShowEffect(true);
-    }
+    const followingNow = !following;
     setFollowing(!following);
+
+    if (followingNow) {
+      shootEffect();
+    }
 
     const result = await fetch(`${endpoint}?id=${user.id}`, {
       method: "POST",
     });
 
     if (!result.ok) {
-      setFollowing(!following);
-      toast.error(`${!following ? "Unfollow" : "Follow"} action failed: ${result.statusText} `);
+      setFollowing(!following); // Revert to the original state
+      toast.error(`${followingNow ? "Unfollow" : "Follow"} action failed: ${result.statusText} `);
     } else {
-      toast.success(`${!following ? "Followed" : "Unfollowed"} Successfully!`, { description: "Finalized on-chain" });
+      toast.success(`${followingNow ? "Followed" : "Unfollowed"} Successfully!`, { description: "Finalized on-chain" });
     }
   };
 
@@ -54,16 +65,14 @@ export const FollowButton = ({ user }: { user: User }) => {
           {following ? "Following" : "Follow"}
         </Button>
 
-        {showEffect && (
-          <Explosion
-            className="absolute left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%] z-[5] select-none pointer-events-none"
-            width={1000}
-            height={1000}
-            globalOptions={{ useWorker: true, disableForReducedMotion: true, resize: true }}
-            decorateOptions={decorateOptions}
-            autorun={{ speed: 3, duration: 1 }}
-          />
-        )}
+        <Explosion
+          onInit={onInitHandler}
+          className="absolute left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%] z-[5] select-none pointer-events-none"
+          width={1000}
+          height={1000}
+          globalOptions={{ useWorker: true, disableForReducedMotion: true, resize: true }}
+          decorateOptions={decorateOptions}
+        />
       </div>
     </>
   );
