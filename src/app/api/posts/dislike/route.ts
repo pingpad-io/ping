fc7e1ade3e59a7cc4c0f13be4ba7a0ba6c78e6f9
kualta <contact@kualta.dev>
2024-06-20 10:24:34 +0000

    add reaction api routes

---
+++ src/app/api/posts/dislike/route.ts
@@ -0,0 +1,56 @@
+import {
+  type CredentialsExpiredError,
+  LensTransactionStatusType,
+  type NotAuthenticatedError,
+  PublicationReactionType,
+  type Result,
+} from "@lens-protocol/client";
+import { type NextRequest, NextResponse } from "next/server";
+import { getLensClient } from "~/utils/getLensClient";
+
+export const dynamic = "force-dynamic";
+
+export async function POST(req: NextRequest) {
+  const { searchParams } = req.nextUrl;
+  const id = searchParams.get("id") || undefined;
+
+  if (!id) {
+    return NextResponse.json({ error: "Missing publication id" }, { status: 400 });
+  }
+
+  try {
+    const { client } = await getLensClient();
+
+    const publication = await client.publication.fetch({
+      forId: id,
+    });
+
+    if (publication.__typename === "Mirror") {
+      return NextResponse.json({ error: "Cannot dislike a share publication" }, { status: 400 });
+    }
+
+    const reactionExists = publication.operations.hasDownvoted;
+
+    let result: Result<void, CredentialsExpiredError | NotAuthenticatedError>;
+    if (reactionExists) {
+      result = await client.publication.reactions.remove({
+        for: id,
+        reaction: PublicationReactionType.Downvote,
+      });
+    } else {
+      result = await client.publication.reactions.add({
+        for: id,
+        reaction: PublicationReactionType.Downvote,
+      });
+    }
+
+    if (result.isFailure()) {
+      return NextResponse.json({ error: result.error.message }, { status: 500 });
+    }
+
+    return NextResponse.json({ result: !reactionExists }, { status: 200 });
+  } catch (error) {
+    console.error("Failed to follow profile: ", error.message);
+    return NextResponse.json({ error: `${error.message}` }, { status: 500 });
+  }
+}
