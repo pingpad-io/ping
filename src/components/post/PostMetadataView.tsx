b47aa7d671d15b42d18da61874d0c8acb02fa89c
kualta <contact@kualta.dev>
2024-06-26 07:23:19 +0000

    implement content view for missing metadata, closes #18

--- src/components/post/PostMetadataView.tsx
+++ src/components/post/PostMetadataView.tsx
@@ -1,12 +1,23 @@
 import type {
   ArticleMetadataV3,
   AudioMetadataV3,
+  CheckingInMetadataV3,
+  EmbedMetadataV3,
+  EventMetadataV3,
   ImageMetadataV3,
+  LinkMetadataV3,
+  LiveStreamMetadataV3,
+  MintMetadataV3,
+  SpaceMetadataV3,
+  StoryMetadataV3,
   TextOnlyMetadataV3,
+  ThreeDMetadataV3,
+  TransactionMetadataV3,
   VideoMetadataV3,
 } from "@lens-protocol/react-web";
 import Markdown from "../Markdown";
 import { VideoPlayer } from "../VideoPlayer";
+import { Badge } from "../ui/badge";
 import type { AnyLensMetadata } from "./Post";
 
 export const getPostMetadataView = (metadata: AnyLensMetadata) => {
@@ -21,17 +32,41 @@ export const getPostMetadataView = (metadata: AnyLensMetadata) => {
       return <VideoView metadata={metadata} />;
     case "AudioMetadataV3":
       return <AudioView metadata={metadata} />;
+    case "LinkMetadataV3":
+      return <LinkView metadata={metadata} />;
+    case "LiveStreamMetadataV3":
+      return <LiveStreamView metadata={metadata} />;
+    case "CheckingInMetadataV3":
+      return <CheckingInView metadata={metadata} />;
+    case "EmbedMetadataV3":
+      return <EmbedView metadata={metadata} />;
+    case "EventMetadataV3":
+      return <EventView metadata={metadata} />;
+    case "MintMetadataV3":
+      return <MintView metadata={metadata} />;
+    case "SpaceMetadataV3":
+      return <SpaceView metadata={metadata} />;
+    case "StoryMetadataV3":
+      return <StoryView metadata={metadata} />;
+    case "TransactionMetadataV3":
+      return <TransactionView metadata={metadata} />;
+    case "ThreeDMetadataV3":
+      return <ThreeDView metadata={metadata} />;
     default:
       return null;
   }
 };
 
+const ContentView = ({ content }: { content: string }) => {
+  return <Markdown content={content} />;
+};
+
 export const TextOnlyView = ({ metadata }: { metadata: TextOnlyMetadataV3 }) => {
-  return <Markdown content={metadata.content} />;
+  return <ContentView content={metadata.content} />;
 };
 
 export const ArticleView = ({ metadata }: { metadata: ArticleMetadataV3 }) => {
-  return <Markdown content={metadata.content} />;
+  return <ContentView content={metadata.content} />;
 };
 
 export const ImageView = ({ metadata }: { metadata: ImageMetadataV3 }) => {
@@ -41,7 +76,7 @@ export const ImageView = ({ metadata }: { metadata: ImageMetadataV3 }) => {
 
   return (
     <div>
-      <Markdown content={metadata.content} />
+      <ContentView content={metadata.content} />
       <div className="relative mt-2 w-full">
         <img src={url} alt={alt || title} className="object-cover border w-full rounded-xl h-auto" />
       </div>
@@ -56,12 +91,64 @@ export const VideoView = ({ metadata }: { metadata: VideoMetadataV3 }) => {
 
   return (
     <div>
-      <Markdown content={metadata.content} />
+      <ContentView content={metadata.content} />
       <VideoPlayer url={url} preview={cover} />
     </div>
   );
 };
 
 export const AudioView = ({ metadata }: { metadata: AudioMetadataV3 }) => {
-  return <Markdown content={metadata.content} />;
+  return <ContentView content={metadata.content} />;
+};
+
+export const LinkView = ({ metadata }: { metadata: LinkMetadataV3 }) => {
+  return (
+    <div>
+      <ContentView content={metadata.content} />
+      <Badge>{metadata.sharingLink}</Badge>
+    </div>
+  );
+};
+
+export const LiveStreamView = ({ metadata }: { metadata: LiveStreamMetadataV3 }) => {
+  return <ContentView content={metadata.content} />;
+};
+
+export const CheckingInView = ({ metadata }: { metadata: CheckingInMetadataV3 }) => {
+  return <ContentView content={metadata.content} />;
+};
+
+export const EmbedView = ({ metadata }: { metadata: EmbedMetadataV3 }) => {
+  return <ContentView content={metadata.content} />;
+};
+
+export const EventView = ({ metadata }: { metadata: EventMetadataV3 }) => {
+  return (
+    <div>
+      <h1>Event</h1>
+      Starts at: <Badge>{metadata.startsAt}</Badge>
+      Ends at: {metadata.endsAt}
+      Where: {metadata.location}
+    </div>
+  );
+};
+
+export const MintView = ({ metadata }: { metadata: MintMetadataV3 }) => {
+  return <ContentView content={metadata.content} />;
+};
+
+export const SpaceView = ({ metadata }: { metadata: SpaceMetadataV3 }) => {
+  return <ContentView content={metadata.content} />;
+};
+
+export const StoryView = ({ metadata }: { metadata: StoryMetadataV3 }) => {
+  return <ContentView content={metadata.content} />;
+};
+
+export const TransactionView = ({ metadata }: { metadata: TransactionMetadataV3 }) => {
+  return <ContentView content={metadata.content} />;
+};
+
+export const ThreeDView = ({ metadata }: { metadata: ThreeDMetadataV3 }) => {
+  return <ContentView content={metadata.content} />;
 };
