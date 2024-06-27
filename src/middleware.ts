9bbea38db114cebacb61fc8dfc5b1edd2ef56f24
kualta <contact@kualta.dev>
2024-06-27 06:47:39 +0000

    fix vercel build

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,12 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import { getLensClient } from "~/utils/getLensClient";
+import { getCookieAuth } from "./utils/getCookieAuth";
 
 export async function middleware(request: NextRequest) {
   // Only run this middleware for the base URL
   if (request.nextUrl.pathname === "/") {
-    const { isAuthenticated } = await getLensClient();
+    const { isAuthenticated } = await getCookieAuth();
 
     if (isAuthenticated) {
       // If authenticated, redirect to /home
