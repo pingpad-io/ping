4a9515aedfa15469e8a9e174fd4fb4c21414aa8f
kualta <contact@kualta.dev>
2024-07-30 22:14:59 +0300

    fix community page

--- src/app/(sidebars)/(title)/communities/page.tsx
+++ src/app/(sidebars)/(title)/communities/page.tsx
@@ -4,28 +4,9 @@ import { CommunityView } from "~/components/communities/CommunityView";
 const endpoint = "/api/community";
 
 const community = async () => {
-  const communities = await getInitialFeed();
 
-  if (!communities) {
-    throw new Error("Failed to get communities");
-  }
-
-  return <Feed ItemView={CommunityView} endpoint={`${endpoint}`} initialData={communities} initialCursor={null} />;
+  return <Feed ItemView={CommunityView} endpoint={`${endpoint}`} initialData={undefined} initialCursor={undefined} />;
 };
 
-const getInitialFeed = async () => {
-  const data = await fetch(`https://pingpad.io${endpoint}`, {
-    method: "GET",
-  })
-    .then((res) => res.json())
-    .catch(() => {
-      console.error("(×_×)⌒☆ Failed to fetch communities");
-      return null;
-    });
-
-  const { communities } = data;
-
-  return communities;
-};
 
 export default community;
