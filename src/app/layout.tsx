e09a20fe6b5b44ee6c5b57af113b0cc218633f5c
kualta <contact@kualta.dev>
2024-08-12 15:11:34 +0300

    remove vercel speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,11 @@
-import { SpeedInsights } from "@vercel/speed-insights/next";
 import dynamic from "next/dynamic";
+import Script from "next/script";
 import { Providers } from "~/components/Providers";
 import { Toaster } from "~/components/ui/sonner";
 import { UserProvider } from "~/components/user/UserContext";
 import { quicksand } from "~/styles/fonts";
 import { getServerAuth } from "~/utils/getServerAuth";
 import "../styles/globals.css";
-import Script from "next/script";
 
 const AuthWatcher = dynamic(() => import("~/components/auth/AuthWatcher"), { ssr: false });
 
@@ -26,12 +25,11 @@ export default async function RootLayout({ children }) {
   return (
     <html className={`${quicksand.variable} scroll-smooth font-sans overflow-x-hidden overflow-y-scroll`} lang="en">
       <head>
-        <Script defer src="https://stats.pingpad.io/script.js" data-website-id="3a604368-ddf7-40a8-a759-05bbe71ce01d"/>
+        <Script defer src="https://stats.pingpad.io/script.js" data-website-id="3a604368-ddf7-40a8-a759-05bbe71ce01d" />
       </head>
       <body className="flex flex-col relative">
         <Providers>
           <UserProvider user={user}>
-            <SpeedInsights />
             <AuthWatcher />
             <Toaster position="top-right" offset={16} />
 
