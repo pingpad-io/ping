78eed06f20fcc2b399aa1c31bef50ebd9fb2a7e7
kualta <contact@kualta.dev>
2025-06-01 21:44:48 +0400

    feat(user): add profile picture viewer

---
+++ src/components/user/AvatarViewer.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { Dialog, DialogContent, DialogTrigger } from "../ui/dialog";
+import type { User } from "./User";
+import { UserAvatar } from "./UserAvatar";
+
+export function AvatarViewer({ user }: { user: User }) {
+  const src = user.profilePictureUrl;
+  const alt = `${user.name || user.handle}'s profile picture`;
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <div className="w-full h-full cursor-pointer">
+          <UserAvatar user={user} link={false} card={false} />
+        </div>
+      </DialogTrigger>
+      {src ? (
+        <DialogContent className="bg-transparent border-none shadow-none p-0 max-w-fit">
+          <img src={src} alt={alt} className="max-h-[90vh] max-w-[90vw] object-contain rounded-xl" />
+        </DialogContent>
+      ) : null}
+    </Dialog>
+  );
+}
