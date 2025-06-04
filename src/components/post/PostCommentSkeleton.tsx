c98307ff635e5c8ace9a7200f409b1f05e524310
kualta <contact@kualta.dev>
2025-06-04 14:29:25 +0400

    update styling

--- src/components/post/PostCommentSkeleton.tsx
+++ src/components/post/PostCommentSkeleton.tsx
@@ -4,24 +4,17 @@ import { Card } from "../ui/card";
 
 export const CommentSkeleton = ({ isLastComment = false }: { isLastComment?: boolean }) => {
   return (
-    <Card>
-      <CardContent className="flex flex-row p-2 min-h-[120px] sm:p-2 sm:pb-4 gap-2">
+    <Card className="min-h-[96px]">
+      <CardContent className="flex flex-row p-2 sm:p-2 sm:pb-4 gap-2">
         <span className="min-h-full flex flex-col justify-start items-center relative">
           <div className="shrink-0 grow-0 rounded-full w-6 h-6 bg-muted animate-pulse" />
-          {/* <div
-            className={`-mt-4 -mr-6 w-full h-[90%] border-l 
-              ${isLastComment && "rounded-full h-[98%]"} 
-              ${!isLastComment && "min-h-[calc(100%+2rem)]"}`}
-          /> */}
         </span>
         <div className="flex w-3/4 shrink group max-w-2xl grow flex-col place-content-start gap-2">
-          {/* PostInfo skeleton */}
           <div className="flex flex-row items-center gap-2">
             <div className="h-3 w-16 bg-muted rounded animate-pulse" />
             <div className="h-3 w-8 bg-muted rounded animate-pulse" />
           </div>
 
-          {/* PostContent skeleton */}
           <div className="space-y-2">
             <div className="h-4 w-full bg-muted rounded animate-pulse" />
             <div className="h-4 w-3/4 bg-muted rounded animate-pulse" />
