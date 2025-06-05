f25aee9d188911078a1e33f8770ecc440d39f02a
kualta <contact@kualta.dev>
2025-06-05 09:37:14 +0400

    update position

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -68,7 +68,7 @@ export function Menu({ isAuthenticated, handle, profileId, user }: MenuClientPro
         <div className="relative w-full h-full flex items-center justify-center">
           <Bell className="w-5 h-5" />
           {newCount > 0 && (
-            <span className="absolute -bottom-1 -right-1 w-3 h-3 rounded-full bg-primary text-primary-foreground text-[9px] flex items-center justify-center font-medium">
+            <span className="absolute -bottom-3 -right-3 w-4 h-4 rounded-full bg-primary text-primary-foreground text-[9px] flex items-center justify-center font-medium">
               {newCount > 9 ? "9+" : newCount}
             </span>
           )}
