d34bedd609239865d3dbc34dd015388e57547047
kualta <contact@kualta.dev>
2025-07-12 17:37:58 +0400

    add timestamp to repost info

--- src/components/post/PostRepostInfo.tsx
+++ src/components/post/PostRepostInfo.tsx
@@ -1,5 +1,6 @@
 import { Repeat2Icon } from "lucide-react";
 import Link from "~/components/Link";
+import { TimeElapsedSince } from "../TimeLabel";
 import type { Post } from "./Post";
 
 export const RepostInfo = ({ post }: { post: Post }) => {
@@ -13,7 +14,10 @@ export const RepostInfo = ({ post }: { post: Post }) => {
       className="flex flex-row items-center gap-1 -mt-2 text-xs font-light leading-3 text-muted-foreground"
     >
       <Repeat2Icon size={14} className="shrink-0 text-muted-foreground" />
-      <span className="pb-0.5">{username} reposted</span>
+      <span className="pb-0.5">
+        {username} reposted{" "}
+        {post.repostedAt && <TimeElapsedSince date={post.repostedAt} />} {" ago"}
+      </span>
     </Link>
   );
 };
